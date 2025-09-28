8e36cc910618e0fa58c144404974256e7dbb6b57
Ankit <lc46377@umbc.edu>
2025-09-28 03:26:05 -0400

    delete agent_frontend

--- agent_frontend/android/app/src/main/kotlin/com/example/agent_frontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.agent_frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- agent_frontend/lib/ui/views/job_page/job_page_viewmodel.dart
+++
@@ -1,3 +0,0 @@
-import 'package:stacked/stacked.dart';
-
-class JobPageViewModel extends BaseViewModel {}
